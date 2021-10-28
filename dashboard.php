58a012c0975521f4112a7f61278e9ead3d023949
minorside99 <nicomaulana.19103@mhs.its.ac.id>
2021-10-28 15:44:34 +0700

    RUBAH NAVBAR

--- dashboard.php
+++ dashboard.php
@@ -25,7 +25,7 @@
     <!-- Navbar -->
   </div>
 </nav>
-    <a class="navbar-brand" href="#">SMART HOME SYSTEM</a>
+    <a class="navbar-brand" href="#">AUTOMATIC TRANSFER SWITCH</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
